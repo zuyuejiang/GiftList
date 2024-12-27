a86e8eae9a5c1eb084a7257d702fda4a15d9271b
zuyuejiang <zuyue.jiang22@imperial.ac.uk>
2024-12-27 17:06:32 +0000

    finished

--- client/index.js
+++ client/index.js
@@ -5,10 +5,17 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // prove to the server we're on the nice list? 
+  const name = 'Norman Block';
+  const index = niceList.findIndex(n => n === name);
+  const merkleTree = new MerkleTree(niceList); 
+  const proof = merkleTree.getProof(index); 
+  // console.log(proof);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    // request body parameters
+    name,
+    proof
   });
 
   console.log({ gift });
